1ef5076c0043c304a3b466b38deb5f914d0b7331
LaraLaPierre <larablapierre@gmail.com>
2018-01-12 21:08:51 -0600

    finished weather.rb and dictionary.rb

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -7,32 +7,67 @@
 
 require "unirest" 
 
-puts "Enter a word"
-puts "=" * 40
-word = gets.chomp
+looking_up_words = true
+
+while looking_up_words
+
+  system "clear"
+
+  puts "Enter a word"
+  puts "=" * 40
+  word = gets.chomp
 
 
-definition_url = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/definitions?limit=10&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+  definition_url = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/definitions?limit=10&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 
-# definition = definition_url.body[0]["text"]
+  definitions = definition_url.body
 
-definition.each.with_index do |definition, index|
-  puts "#{index + 1}. #{definition["text"]}"
   puts 
+  puts
+  puts "DEFINITIONS"
+  puts "=" * 40
+
+  definitions.each.with_index do |definition, index|
+    puts "#{index + 1}. #{definition["text"]}"
+    puts 
+  end 
+
+  puts 
+  puts 
+
+  top_example_url = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/topExample?useCanonical=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+
+  top_example = top_example_url.body["text"]
+
+
+  puts "TOP EXAMPLE"
+  puts "=" * 40
+  puts top_example
+
+  puts
+  puts 
+
+  pronunciation_url = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5
+  ")
+  pronunciations = pronunciation_url.body
+
+
+  puts "PRONOUNCIATIONS"
+  puts "=" * 40
+
+  pronunciations.each.with_index do |pronunciation, index|
+    puts "#{index + 1}. #{pronunciation["raw"]}"
+    puts 
+    end
+
+  puts "Press Enter to look up another word, otherwise press q to exit"
+  user_input = gets.chomp
+
+  if user_input == "q"
+    looking_up_words = false
+  end
 end 
+system "clear"
+puts "Thanks for looking up words with me."
 
-top_example_url = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{top_example}/topExample?useCanonical=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 
-top_example = top_example_url.body[0]["text"]
-
-pronunciation_url = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5
-")
-
-pronunciation = pronunciation_url.body[0]["raw"]
-
-puts definition
-puts "=" * 40 
-puts top_example
-# puts "=" * 40 
-# puts pronunciation
-# puts "=" * 40 
